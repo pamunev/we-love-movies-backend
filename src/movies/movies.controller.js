961de49e37e32fba5f7a1c3e69889cc4407b62ed
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-08-02 20:35:46 -0500

    Integer to increments in theaters table

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,4 +1,12 @@
 // Where do I start?
 /* 
 - First I do a simple list function.
+- Let's just give it a go. 
+- What stuff do I need to import?
 */
+
+const movies = require
+
+function list(req, res, next) {
+    res.json({ data: movies })
+}
