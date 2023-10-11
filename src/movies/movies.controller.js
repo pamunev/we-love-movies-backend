70491ceadf105ff02452283a17e764bd2b454967
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-10-11 16:24:48 -0500

    Cleaned up comments and unnecessary imports in movies and reviews components.

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,54 +1,53 @@
-// Where do I start?
-/* 
-- First I do a simple list function.
-- Let's just give it a go. 
-- What stuff do I need to import?
-*/
-
 const moviesService = require("./movies.service");
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function movieExists(req, res, next) {
-    const movie = await moviesService.read(req.params.movieId)
-    if (movie) {
-        res.locals.movie = movie;
-        return next()
-    }
-    next({ status: 404, message: `Movie cannot be found` })
+  const movie = await moviesService.read(req.params.movieId);
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: `Movie cannot be found` });
 }
 
 function read(req, res) {
-    const { movie: data } = res.locals
-    res.json({ data })
+  const { movie: data } = res.locals;
+  res.json({ data });
 }
 
 async function list(req, res, next) {
-    const isShowing = req.query.is_showing
-    
-    let movies
-    if (isShowing) {
-        movies = await moviesService.listShowing()
-    } else {
-        movies = await moviesService.list();
-    }
-    res.json({ data: movies })
+  const isShowing = req.query.is_showing;
+
+  let movies;
+  if (isShowing) {
+    movies = await moviesService.listShowing();
+  } else {
+    movies = await moviesService.list();
+  }
+  res.json({ data: movies });
 }
 
 async function listTheatersShowingMovie(req, res, next) {
-    const movieId = req.params.movieId
-    const theaters = await moviesService.listTheatersShowingMovie(movieId)
-    res.json({ data: theaters })
+  const movieId = req.params.movieId;
+  const theaters = await moviesService.listTheatersShowingMovie(movieId);
+  res.json({ data: theaters });
 }
 
 async function listReviewsForMovie(req, res, next) {
-    const movieId = req.params.movieId
-    const reviews = await moviesService.listReviewsForMovie(movieId)
-    res.json({ data: reviews })
+  const movieId = req.params.movieId;
+  const reviews = await moviesService.listReviewsForMovie(movieId);
+  res.json({ data: reviews });
 }
 
 module.exports = {
-    list: asyncErrorBoundary(list),
-    read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
-    listTheatersShowingMovie: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listTheatersShowingMovie)],
-    listReviewsForMovie: [asyncErrorBoundary(movieExists), asyncErrorBoundary(listReviewsForMovie)],
-}
+  list: asyncErrorBoundary(list),
+  read: [asyncErrorBoundary(movieExists), asyncErrorBoundary(read)],
+  listTheatersShowingMovie: [
+    asyncErrorBoundary(movieExists),
+    asyncErrorBoundary(listTheatersShowingMovie),
+  ],
+  listReviewsForMovie: [
+    asyncErrorBoundary(movieExists),
+    asyncErrorBoundary(listReviewsForMovie),
+  ],
+};
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,38 +1,34 @@
-const knex = require("../db/connection")
-const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
-const mapProperties = require("../utils/map-properties")
+const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
 const addCritic = mapProperties({
-    preferred_name: "critic.preferred_name",
-    surname: "critic.surname",
-    organization_name: "critic.organization_name",
-  });
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+});
 
 function read(reviewId) {
-    return knex("reviews as r")
-      .join("critics as c", "c.critic_id", "r.critic_id")
-      .select("r.*", "c.*")
-      .where({ "review_id": reviewId })
-      .first()
-      .then(addCritic)
+  return knex("reviews as r")
+    .join("critics as c", "c.critic_id", "r.critic_id")
+    .select("r.*", "c.*")
+    .where({ review_id: reviewId })
+    .first()
+    .then(addCritic);
 }
 
 async function update(updatedReview) {
-    return knex("reviews")
-      .select("*")
-      .where({ "review_id": updatedReview.review_id })
-      .update(updatedReview, "*")
+  return knex("reviews")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview, "*");
 }
 
 function destroy(reviewId) {
-    return knex("reviews")
-      .where({ "review_id": reviewId })
-      .del()
+  return knex("reviews").where({ review_id: reviewId }).del();
 }
-
 
 module.exports = {
-    read,
-    update,
-    destroy,
-}
+  read,
+  update,
+  destroy,
+};
