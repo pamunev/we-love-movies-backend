cfa92e45245bd49ecc99f5f4600163fb49cb96b5
Pablo Munoz-Evers <117239858+pamunev@users.noreply.github.com>
2023-10-11 16:06:26 -0500

    About to clean up comments.

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,7 +1,6 @@
 const router = require("express").Router({ mergeParams: true })
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed")
-//const theatersRouter = require("../theaters/theaters.router")
 
 router.route("/:movieId/reviews").get(controller.listReviewsForMovie).all(methodNotAllowed)
 router.route("/:movieId/theaters").get(controller.listTheatersShowingMovie).all(methodNotAllowed)
@@ -9,11 +8,3 @@ router.route("/").get(controller.list).all(methodNotAllowed)
 router.route("/:movieId").get(controller.read).all(methodNotAllowed)
 
 module.exports = router;
-
-/*
-For /movies/:movieId/theaters , I think I'll have to write the 
-middleware etc. in theaters.controllers. And then I'll import
-it from here.
-Because I'm basically doing a list() for theaters, but only
-for theaters that are playing :movieId. 
-*/
